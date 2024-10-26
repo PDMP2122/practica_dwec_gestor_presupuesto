ac3fa5db4ea5ec2cccb86b6b78e0f17bff690978
Pablo de Madaria Pascual <pablodemadaria@gmail.com>
2024-10-26 20:14:36 +0200

    creados ficheros nuevos, import en html y export con funciones vacias en gestionPresupuestoWeb

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,13 @@
+
+function mostrarDatoEnId(){}
+
+function mostrarGastoWeb(){}
+
+function mostrarGastosAgrupadosWeb(){}
+
+
+export{
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
