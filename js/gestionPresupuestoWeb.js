12774eea85b7af450b038e6ef63903596d606e2c
Pablo de Madaria Pascual <pablodemadaria@gmail.com>
2024-10-30 21:42:45 +0100

    Funcion repintar

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -65,8 +65,30 @@ function mostrarGastosAgrupadosWeb(idElemento, agroup, periodo){
 }
 
 
+/*
+Por tanto, es necesario disponer de una función que *vuelva a crear toda la estructura HTML* que refleje los cambios realizados en el modelo de datos. Esta función se denominará ~repintar~, y realizará las siguientes tareas:
+     - Mostrar el presupuesto en ~div#presupuesto~ (funciones ~mostrarPresupuesto~ y ~mostrarDatoEnId~)
+     - Mostrar los gastos totales en ~div#gastos-totales~ (funciones ~calcularTotalGastos~ y ~mostrarDatoEnId~)
+     - Mostrar el balance total en ~div#balance-total~ (funciones ~calcularBalance~ y ~mostrarDatoEnId~)
+     - *Borrar el contenido* de ~div#listado-gastos-completo~, para que el paso siguiente no duplique la información. Puedes utilizar ~innerHTML~ para borrar el contenido de dicha capa.
+     - Mostrar el listado completo de gastos en ~div#listado-gastos-completo~ (funciones ~listarGastos~ y ~mostrarGastoWeb~)
+     
+     La función ~repintar~ *no actualizará el resto de capas* (filtrados y agrupaciones) de la práctica anterior (lo haremos así por simplicidad).
+*/
+
+function repintar(){
+
+  mostrarDatoEnId("div#presupuesto", gestionPresupuesto.mostrarPresupuesto());
+  mostrarDatoEnId("div#gastos-totales", gestionPresupuesto.calcularTotalGastos());
+  mostrarDatoEnId("div#balance-total", gestionPresupuesto.calcularBalance());
+  document.querySelector("div#listado-gastos-completo").innerHTML="";
+  mostrarGastoWeb("div#listado-gastos-completo", gestionPresupuesto.listarGastos());
+
+}
+
 export{
     mostrarDatoEnId,
     mostrarGastoWeb,
-    mostrarGastosAgrupadosWeb
+    mostrarGastosAgrupadosWeb,
+    repintar
 }
