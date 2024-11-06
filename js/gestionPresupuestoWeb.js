6d1feb944c3fda90d816c498e9ff9bfbda04eaa9
Pablo de Madaria Pascual <pablodemadaria@gmail.com>
2024-11-06 19:55:08 +0100

    Funcion BorrarHandle

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -55,17 +55,27 @@ function mostrarGastoWeb(idElemento, gasto){
     editarManejador.gasto = gasto;
     //Se añade manejador al botón
     botonEditar.addEventListener("click", editarManejador)
-
-
+    //Se añade el boton
     divGasto.append(botonEditar);
 
-
-
+    /*Boton borrar gasto */
+    
+    let botonBorrar = document.createElement("button");
+    botonBorrar.setAttribute("type", "button")
+    botonBorrar.classList.add("gasto-borrar");
+    botonBorrar.textContent="Borrar";
+   
+    //Manejador de enventos boton borrar
+    let borrarManejador = Object.create(BorrarHandle);
+    //Asocio borrarManejador a gasto
+    borrarManejador.gasto = gasto;
+    //Se añade manejador al botón
+    botonBorrar.addEventListener("click", borrarManejador)
+    //Se añade el boton
+    divGasto.append(botonBorrar);
 
 
     elemento.append(divGasto);
-
-
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agroup, periodo){
@@ -129,37 +139,6 @@ let botonNuevoGasto = document.getElementById("anyadirgasto");
 botonNuevoGasto.addEventListener("click", nuevoGastoWeb);
 
 
-/*
-**** Función ~EditarHandle~
-     Esta función se utilizará como [[https://es.javascript.info/introduction-browser-events#objetos-handlers-handleevent][objeto manejador de eventos]] para editar un gasto.
-
-     La razón de utilizar esta función es la siguiente: queremos añadir un botón para *editar* cada gasto de la lista. Por tanto, necesitamos que haya una *conexión* entre 
-     dicho botón y el gasto asociado a él. Recordemos que si tenemos 4 gastos, tendremos 4 botones de Editar.
-
-     Hay muchas soluciones para realizar esta asociación: una podría ser añadir un ~id~ al botón ~Editar~ de dicho gasto que sea el mismo que el ~id~ del gasto asociado.
-      Así, si se hace clic en dicho botón, inspeccionando su atributo ~id~ podremos saber el gasto al que hace referencia. No obstante, esta solución no es demasiado buena:
-       la lógica de negocio de nuestra aplicación solo almacena el array de gastos, por lo que tendríamos que añadir una función para buscar dicho gasto dado su ~id~.
-
-     En lugar de ello optaremos por una solución un poco más elaborada aprovechando que ~addEventListener~ nos permite
-      [[https://es.javascript.info/introduction-browser-events#objetos-handlers-handleevent][utilizar un objeto]] como manejador de eventos.
-
-     La función ~EditarHandle~ será una [[https://es.javascript.info/constructor-new#funcion-constructora][función constructora]] que definirá exclusivamente un 
-     método llamado ~handleEvent~. Cuando creemos un objeto basado en su prototipo, asignaremos a dicho objeto una propiedad llamada ~gasto~, que será una referencia 
-     al gasto que estemos editando. El código de la función ~handleEvent~ podrá hacer referencia a dicho gasto a través de ~this.gasto~, ya que es una propiedad del 
-     objeto. Esta función realizará las siguientes tareas:
-     - Pedir al usuario *la información necesaria para editar el gasto* mediante sucesivas preguntas con [[https://es.javascript.info/alert-prompt-confirm][prompt]]. 
-     Por simplicidad, de momento *no se comprobará la validez de dichos datos*. La fecha vendrá dada en formato internacional (~yyyy-mm-dd~) y las *etiquetas* se 
-     introducirán en un único cuadro de texto como una *lista separada por comas* (por ejemplo, ~etiqueta1,etiqueta2,etiqueta3~). Recuerda que ~prompt~ admite como 
-     segundo parámetro el valor por defecto del cuadro de diálogo, por lo que puedes proporcionar el valor actual de cada propiedad del gasto.
-     - Convertir el valor a número (recuerda que ~prompt~ siempre devuelve un ~string~).
-     - Convertir la cadena de texto de etiquetas devuelta por ~prompt~ [[https://es.javascript.info/array-methods#split-y-join][a un array]].
-     - Actualizar las propiedades del gasto (disponible mediante ~this.gasto~), mediante las funciones ~actualizarValor~, ~actualizarDescripcion~, ~actualizarFecha~ y 
-     ~anyadirEtiquetas~.
-     - Llamar a la función ~repintar~ para que se muestre la lista de gastos con los datos actualizados de la edición.
-     
-
-*/
-
 
 /*Funciones manejadoras eventos botones */
 
@@ -175,6 +154,14 @@ let EditarHandle = {
     this.gasto.actualizarFecha(fecha);
     this.gasto.anyadirEtiquetas(...etiquetas);
 
+    repintar();
+  }
+}
+
+let BorrarHandle = {
+  handleEvent: function(evento){
+
+    gestionPresupuesto.borrarGasto(this.gasto.id);
     repintar();
   }
 }
